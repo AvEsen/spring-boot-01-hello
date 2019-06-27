38cd93170444c092c5778628fbf00bd1a4a4bd4a
AvEsen <18166413733@163.com>
2019-06-27 17:25:21 +0800

    huiche1

--- src/main/java/com/mengxuegu/springboot/HelloMainApplication.java
+++ src/main/java/com/mengxuegu/springboot/HelloMainApplication.java
@@ -30,6 +30,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloMainApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(HelloMainApplication.class, args);
     }
